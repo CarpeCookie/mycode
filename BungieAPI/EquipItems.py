de93b787166a9530dc63d84d09fe1020e5d8e33a
MatthewMakowicki <matthewmakowicki@gmail.com>
2021-07-09 03:44:15 +0000

    More Bungo

--- BungieAPI/EquipItems.py
+++ BungieAPI/EquipItems.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+import json 
 
 HEADERS = {"X-API-Key":'c6b881de35a143f98cda3d1e5dbf27b4'}
 
@@ -10,14 +11,16 @@ Path: /Destiny2/Actions/Items/EquipItems/
 Equip a list of items by itemInstanceIds. You must have a valid Destiny Account, and either be in a social space, in orbit, or offline. Any items not found on your character will be ignored.
 """
 
-data = {
-"itemIds":[6917529368069022675, 6917529367937429262],
+text_equip_item = {
+"itemId": 6917529368069022675,
 "characterId": 4611686018465168592,
 "membershipType": 2
 }
 
+equip_payload = json.dumps(text_equip_item)
+
 try:
-    requests.post("https://www.bungie.net/platform/Destiny2/Actions/Items/EquipItems/", json=data, headers=HEADERS)
+    r = session.post("https://www.bungie.net/platform/Destiny2/Actions/Items/EquipItem/", data=equip_payload, headers=HEADERS)
     print("success")
 except:
     print("failure")
--- BungieAPI/TokenTest.py
+++ BungieAPI/TokenTest.py
@@ -4,17 +4,19 @@ import requests
 
 HEADERS = {"X-API-Key":'c6b881de35a143f98cda3d1e5dbf27b4'}
 
+
 """
 Verb: POST
 Path: /Destiny2/Actions/Items/EquipItems/
 Equip a list of items by itemInstanceIds. You must have a valid Destiny Account, and either be in a social space, in orbit, or offline. Any items not found on your character will be ignored.
 """
 
-data= {
-"Authorization":[],
-"Content-Type": mustbeint_X,
-"grant_type=authoization": mustbeint_Y,
-"membershipType": mustbeint_Z
-}
+data = {'grant_type': 'authorization_code', 'code': '6b881de35a143f98cda3d1e5dbf27b4'}
 
-r = requests.post("https:i//www.bungie.net/platform/Destiny2/Actions/Items/EquipItems/", json=data, headers=HEADERS)
+
+r = requests.post("https://www.bungie.net/Platform/App/OAuth/token/", json=data, headers=HEADERS)
+try:
+    print(r)
+except:
+    r = r.json()
+    print(r)
