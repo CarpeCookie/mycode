ec5f426ea799eab596a2e566f960833f73daf4bf
MatthewMakowicki <matthewmakowicki@gmail.com>
2021-07-09 18:27:41 +0000

    more changes

--- BungieAPI/AuthTest.py
+++ BungieAPI/AuthTest.py
@@ -6,8 +6,6 @@ try:
     r = requests.get("https://www.bungie.net/en/OAuth/Authorize?client_id=37057&response_type=code&state=bdOqDW-9po.QSWkwiHOpyA9HTN2vSwbP3PNy35pDuZc", headers=HEADERS)    
     x = r.text
     print(x)
-    with open("test.txt", "a") as test: 
-        print(x, file=test)
 
 except:
     print("failure")
