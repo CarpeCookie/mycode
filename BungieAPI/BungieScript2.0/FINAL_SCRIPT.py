42dac54055d5bb9eddbafd0b7279cf22b2e644c9
MatthewMakowicki <matthewmakowicki@gmail.com>
2021-07-14 04:58:54 +0000

    100% complete

--- BungieAPI/BungieScript2.0/FINAL_SCRIPT.py
+++ BungieAPI/BungieScript2.0/FINAL_SCRIPT.py
@@ -64,8 +64,8 @@ def Equipment_Menu():
         Print_Gear()
     elif Equipped_Gear == "2":
         Print_Stats()
- #   elif Equipped_Gear == "3":
- #       Check_Fashion()
+    elif Equipped_Gear == "3":
+        Check_Fashion()
     elif Equipped_Gear == "4":
         Main_Menu()
     elif Equipped_Gear == "5":
@@ -1260,7 +1260,106 @@ def Clan_Raids():
         Clan_Raids()
 
 
+def Check_Fashion():
+    print("\nHow's your fashion sense?\n1: Warlock\n2: Hunter\n3: Titan\n4: Equipment Menu\n5: Main Menu\n6: Quit\n")
+    Fashion_Sense = input()
+    if Fashion_Sense == "1":
+        print("\nYour look could use some work.\n")
+        Check_Fashion()
+    if Fashion_Sense == "2":
+        Hunter_Fashion()
+    if Fashion_Sense == "3":
+        Titan_Fashion()
+    if Fashion_Sense == "4":
+        Equipment_Menu()
+    if Fashion_Sense == "5":
+        Main_Menu()
+    elif Fashion_Sense == "6":
+        quit()
+    else:
+        print("\n \nInvalid Input. Try Again.\n \n")
+        Check_Fashion()
 
+def Hunter_Fashion():    
+    gear = requests.get("https://www.bungie.net/Platform/Destiny2/2/Profile/" + membershipId + "/Character/" + hunterid + "/?components=205", headers=HEADERS)
+    gear = gear.json()
+    gear = gear['Response']['equipment']['data']['items']
+    helm = gear[3]['itemInstanceId']
+    arms = gear[4]['itemInstanceId']
+    chest = gear[5]['itemInstanceId']
+    legs = gear[6]['itemInstanceId']
+    class_item = gear[7]['itemInstanceId']
+
+
+    scooby_hat = "6917529211628824139"
+    scooby_arms = "6917529211602451858"
+    scooby_chest = "6917529211620260636"
+    scooby_legs = "6917529211620255407"
+    scooby_snack = "6917529194764422250"
+#These values are tied to a specific item instance.  If I change the shader on the item, the hash will stay the same, even though the item will look different.  I don't know where the info on the specific shader is located, so I can't use that as the value.  This is really just to show I can check for specific item sets.
+
+
+    if helm == scooby_hat:
+        if arms == scooby_arms:
+            if chest == scooby_chest:
+                if legs == scooby_legs:
+                    if class_item == scooby_snack:
+                        print("\nLooks like we've got another mystery on our hands!\n")
+                        Check_Fashion()
+                    else:
+                        print("\nYour look could use some work.\n")
+                        Check_Fashion()
+                else:
+                    print("\nYour look could use some work.\n")
+                    Check_Fashion()
+            else:
+                print("\nYour look could use some work.\n")
+                Check_Fashion()
+        else:
+            print("\nYour look could use some work.\n")
+            Check_Fashion()
+    else:
+        print("\nYour look could use some work.\n")
+        Check_Fashion()
+
+def Titan_Fashion():
+    gear = requests.get("https://www.bungie.net/Platform/Destiny2/2/Profile/" + membershipId + "/Character/" + titanid + "/?components=205", headers=HEADERS)
+    gear = gear.json()
+    gear = gear['Response']['equipment']['data']['items']
+    helm = gear[3]['itemInstanceId']
+    arms = gear[4]['itemInstanceId']
+    chest = gear[5]['itemInstanceId']
+    legs = gear[6]['itemInstanceId']
+    class_item = gear[7]['itemInstanceId']
+
+    ogre_head = "6917529211829354390"
+    ogre_arms = "6917529139640021014"
+    ogre_chest = "6917529211978348615"
+    ogre_legs = "6917529211823664235"
+    ogre_onion = "6917529211984649722"
+
+    if helm == ogre_head:
+        if arms == ogre_arms:
+            if chest == ogre_chest:
+                if legs == ogre_legs:
+                    if class_item == ogre_onion:
+                        print("\nThat'll do, Donkey. That'll do.\n")
+                        Check_Fashion()
+                    else:
+                        print("\nYour look could use some work.\n")
+                        Check_Fashion()
+                else:
+                    print("\nYour look could use some work.\n")
+                    Check_Fashion()
+            else:
+                print("\nYour look could use some work.\n")
+                Check_Fashion()
+        else:
+            print("\nYour look could use some work.\n")
+            Check_Fashion()
+    else:
+        print("\nYour look could use some work.\n")
+        Check_Fashion()
 
 Main_Menu()
 #Call the Main_Menu function to start the script
